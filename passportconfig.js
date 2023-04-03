7f1be265f7c63fab69fc9395818c9cb5cc003f27
Baz Murphy <bazmurphy@gmail.com>
2023-04-03 16:09:46 +0100

    initial commit

---
+++ passportconfig.js
@@ -0,0 +1,38 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+function initialise(passport, getUserByEmail, getUserById) {
+  // we have a function that authenticates the user
+  async function authenticateUser(email, password, done) {
+    const user = getUserByEmail(email);
+    if (user == null) {
+      // done 1st parameter is the error, 2nd is the user we found, 3rd is message
+      // we don't have an error setup, and we found no user
+      return done(null, false, { message: 'No user with that email' });
+    }
+
+    try {
+      // we compare the password our user sent in on login, and the stored password
+      if (await bcrypt.compare(password, user.password)) {
+        // we return the user that we want to autheticate with (the user they have logged in as)
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Password incorrect' });
+      }
+    } catch (error) {
+      return done(error);
+    }
+  }
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+  passport.serializeUser((user, done) => {
+    // we get the users id and save that into our session
+    // so we pass it into the done function, and null is the error, and user.id is our user
+    return done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    // https://stackoverflow.com/questions/27637609/understanding-passport-serialize-deserialize
+    return done(null, getUserById(id));
+  });
+}
+
+module.exports = initialise;
---
+++ server.js
@@ -0,0 +1,131 @@
+if (process.env.NODE_ENV !== 'production') {
+  // this loads in all our environment variables and sets them inside process.env
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('express-flash');
+const methodOverride = require('method-override');
+
+const PORT = 3000 || process.env.PORT;
+
+const initalisePassport = require('./passportconfig');
+
+initalisePassport(
+  // this is the passport that we are configuring
+  passport,
+  // this is function getUserByEmail that we use to find our user based on the email
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+
+const users = [];
+
+app.set('view-engine', 'ejs');
+
+// this parses the body from an html form
+// so we can access them inside of the request
+app.use(express.urlencoded({ extended: false }));
+
+// this is for flash messages
+app.use(flash());
+
+// this is for user sessions
+app.use(
+  session({
+    // the first option is a secret
+    secret: process.env.SESSION_SECRET,
+    // should we re-save our session vairiables if nothing has changed
+    resave: false,
+    // do you want to save an empty value in the session if there is no value
+    saveUninitialized: false,
+  })
+);
+
+// initialize is a function inside passport which sets up the basics for us
+app.use(passport.initialize());
+
+// we want to store our variables to be persisted across the entire session our user has
+// which will work with the app.use(session({})) above
+app.use(passport.session());
+
+// use method-override to allow us to understand a delete request from a form
+// we pass what we want our method override to be, this is what we will pass for our method
+app.use(methodOverride('_method'));
+
+app.get('/', checkIsAuthenticated, (req, res) => {
+  // with sessions, the req.user is always going to be sent with the user that is authenticated
+  res.render('index.ejs', { name: req.user.name });
+});
+
+app.get('/login', checkIsNotAuthenticated, (req, res) => {
+  res.render('login.ejs');
+});
+
+// we don't need the (req, res) function anymore, we use the passport authentication middleware
+// app.post('/login', (req, res) => {});
+// we want to use the local Strategy
+// and pass it a list of options for things we want to modify
+app.post(
+  '/login',
+  checkIsNotAuthenticated,
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    // this sets the flash message on failure which is defined in passportconfig.js in the format { message: "bla bla" }
+    failureFlash: true,
+  })
+);
+
+app.get('/register', checkIsNotAuthenticated, (req, res) => {
+  res.render('register.ejs');
+});
+
+app.post('/register', checkIsNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect('/login');
+  } catch (error) {
+    res.redirect('/register');
+  }
+  console.log(users);
+});
+
+app.delete('/logout', (req, res) => {
+  // passport sets up this logOut function automatically, it will clear the session and log our user out
+  req.logOut();
+  res.redirect('/login');
+});
+
+// this is our own middleware function, it takes a req, a res, and a next which we call whenever we are done
+function checkIsAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    // if they are authenticated, we want to continue
+    return next();
+  }
+  // if they are not authenticated redirect them to the login page
+  res.redirect('/login');
+}
+
+function checkIsNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    // if they are authenticated redirect them to the home page
+    return res.redirect('/');
+  }
+  // if they are not authenticated, we want to continue
+  next();
+}
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
